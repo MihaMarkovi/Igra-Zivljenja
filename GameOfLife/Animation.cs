7436deb0fae1fd90cafc2eef02c6f0962f620ba0
MihaMarkovi <miha0552@gmail.com>
2022-06-03 16:57:59 +0200

    Deleted unnecessary files and libraries.

--- GameOfLife/Animation.cs
+++ GameOfLife/Animation.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using System.Threading;
 
 
 namespace GameOfLife
